0b7affab89ae205898cd8f2dc12bb51bb0e1125d
Aarni Koskela <akx@iki.fi>
2010-08-02 23:59:02 +0300

    snip snip

--- game.js
+++ game.js
@@ -144,7 +144,22 @@ function doubleSizeOn() {
 	addMessage("Whoomph! Everything seems... bigger.");
 }
 
+function writeHTML() {
+	document.body.innerHTML = '<table>\
+	<tr>\
+		<td><canvas id="game" width="400" height="480"></canvas></td>\
+		<td><div id="log"></div></td>\
+	</tr>\
+	</table>\
+	<br /><small>\
+	<del>Beta</del> <del>Alpha</del> Pre-alpha version. Use numpad to move.<br />\
+	Inspiration <i>&amp;</i> sprites pilfered from <a href="http://www.qcfdesign.com/?cat=20">Desktop Dungeons</a>.\
+	</small>\
+	';
+}
+
 function init() {
+	writeHTML();
 	canvas = document.getElementById("game");
 	document.addEventListener("keyup", keyPress, false);
 	ctx = canvas.getContext('2d');
