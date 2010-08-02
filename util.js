ff0c0f9404e0b097975f954f4f2f7daad108eab8
akx <akx@localhost>
2010-08-02 10:50:31 +0300

    pbperlp

--- util.js
+++ util.js
@@ -6,3 +6,8 @@ function irnd(a, b) {
 	if(a === undefined || b === undefined) throw "Hissy fit";
 	return Math.floor(rnd(a, b));
 }
+
+function log(message) {
+	var args = arguments;
+	console.log.apply(null, args);
+}
