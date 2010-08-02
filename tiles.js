7fb94f898bfe51233b9e07be6948818e0fc5ad24
akx <akx@localhost>
2010-08-02 15:30:09 +0300

    parempi parempi

--- tiles.js
+++ tiles.js
@@ -1,12 +1,19 @@
 
 
 var tiles = {};
+var tilesToLoad = 0;
+var allTilesLoaded = false;
 function loadTiles() {
+
 	_.each(window._tileData, function(tileData, tile) {
+		tilesToLoad ++;
 		tiles[tile] = _.extend(new Image(), {
 			src: 'data:image/png;base64,'+tileData,
-			onLoad: function() {log("OK: " + tile);
-		}});
+			onload: function() {
+				tilesToLoad--;
+				if(tilesToLoad == 0) allTilesLoaded = true;
+			}
+		});
 	});
 	delete window._tileData;
 	/*
