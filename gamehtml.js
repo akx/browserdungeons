9e7f52e1473e4a327b3a605ce654bd130428a3be
akx <akx@localhost>
2010-08-03 01:51:01 +0300

    better metter

--- gamehtml.js
+++ gamehtml.js
@@ -6,7 +6,11 @@ function writeHTML() {
 <td><div id="log"></div></td>\
 </tr>\
 </table>\
-<br /><small><del>Beta</del> <del>Alpha</del> Pre-alpha version. Use numpad to move.<br />\
+<br />\
+<a href="#" onclick="doubleSizeOn(); return false">Double size on...</a> \
+<a href="#" onclick="doubleSizeOff(); return false">Double size off!</a> \
+<a href="#" onclick="startGame(); return false">Rewind!</a> <br />\
+<del>Beta</del> <del>Alpha</del> Pre-alpha version. Use numpad to move; H and M to use health and mana potions.<br />\
 Inspiration <i>&amp;</i> sprites pilfered from <a href="http://www.qcfdesign.com/?cat=20">Desktop Dungeons</a>.\
-</small>';
+';
 }
