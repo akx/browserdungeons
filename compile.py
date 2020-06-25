5bc18d7ab0745029e25ed74c98239705ec7a49f9
Aarni Koskela <akx@iki.fi>
2020-06-25 23:11:06 +0300

    Modernize compile.py and add closure compiler

--- compile.py
+++ compile.py
@@ -1,28 +1,28 @@
 import glob, os, sys
-compilerJars = (
-	r"\dev\compiler.jar",
-	r"c:\utils\closure-compiler.jar",
-)
+
+compilerJars = ("./tools/closure-compiler-v20200614.jar",)
 for compiler in compilerJars:
-	if os.path.exists(compiler):
-		break
+    if os.path.exists(compiler):
+        break
 else:
-	print "No closure compiler found. Dying."
-	sys.exit(1)
+    print("No closure compiler found. Dying.")
+    sys.exit(1)
 coreJs = ["tiledata.js", "underscore.js", "Base.js", "classes.js", "util.js"]
 skipJs = []
 finalJs = ["game.js"]
-otherJs = sorted(set(f for f in glob.glob("*.js") if f[0] != "_") - set(coreJs) - set(skipJs) - set(finalJs))
+baseJs = {f for f in glob.glob("*.js") if f[0] != "_"}
+otherJs = sorted(baseJs - set(coreJs) - set(skipJs) - set(finalJs))
 allJs = coreJs + otherJs + finalJs
 jsSize = sum(os.stat(f).st_size for f in allJs)
-cmdLine = ["java -jar %s" % compiler]
+cmdLine = [f"java -jar {compiler}"]
 for js in allJs:
-	cmdLine.append("--js %s" % js)
+    cmdLine.append(f"--js {js}")
 cmdLine.append("--js_output_file _compiled.js")
 cmdLine = " ".join(cmdLine)
 os.system(cmdLine)
 cSize = os.stat("_compiled.js").st_size
 
-print "Original: %d bytes" % jsSize
-print "Compiled: %d bytes" % cSize
-print "   Ratio: %.1f%%" % ( cSize / float(jsSize) * 100)
+ratio = cSize / float(jsSize) * 100
+print(f"Original: {jsSize} bytes")
+print(f"Compiled: {cSize} bytes")
+print(f"   Ratio: {ratio:.1f}%")
